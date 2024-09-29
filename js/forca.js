9aedfab4cd054c028a9697b1fdf6a9039e90c5dc
Luis Gustavo B. Santiago <74880596+Phelps671@users.noreply.github.com>
2024-09-28 22:05:55 -0300

    Update forca.js

--- js/forca.js
+++ js/forca.js
@@ -1,349 +1,150 @@
 let tentativas = 6;
-var a=0;
 let listaDinamica = [];
 let listaLetraErrada = [];
 let palavraSecretaCategoria;
 let palavraSecretaSorteada;
+let letrasErradas = document.getElementById("letrasErradas");
 
 const palavras = [
-    palavra001 = {
-        nome: "IRLANDA",
-        categoria:"LUGARES"
-    },
-    palavra002 = {
-        nome: "EQUADOR",
-        categoria:"LUGARES"
-    },
-    palavra003 = {
-        nome: "CHILE",
-        categoria:"LUGARES"
-    },
-    palavra004 = {
-        nome: "INDONESIA",
-        categoria:"LUGARES"
-    },
-    palavra005 = {
-        nome: "MALDIVAS",
-        categoria:"LUGARES"
-    },
-    palavra006 = {
-        nome: "INGLATERRA",
-        categoria:"LUGARES"
-    },
-    palavra007 = {
-        nome: "GROELANDIA",
-        categoria:"LUGARES"
-    },
-    palavra008 = {
-        nome: "UZBEQUISTAO",
-        categoria:"LUGARES"
-    },
-    palavra009 = {
-        nome: "INDONESIA",
-        categoria:"LUGARES"
-    },
-    palavra010 = {
-        nome: "CREGUENHEM",
-        categoria:"LUGARES"
-    },
-    palavra011 = {
-        nome: "BICICLETA",
-        categoria:"TRANSPORTE"
-    },
-    palavra012 = {
-        nome: "LANCHA",
-        categoria:"TRANSPORTE"
-    },
-    palavra013 = {
-        nome: "NAVIO",
-        categoria:"TRANSPORTE"
-    },
-    palavra014 = {
-        nome: "TELEFERICO",
-        categoria:"TRANSPORTE"
-    },
-    palavra015 = {
-        nome: "MOTOCICLETA",
-        categoria:"TRANSPORTE"
-    },
-    palavra016 = {
-        nome: "BARCO",
-        categoria:"TRANSPORTE"
-    },
-    palavra017 = {
-        nome: "AERONAVE",
-        categoria:"TRANSPORTE"
-    },
-    palavra018 = {
-        nome: "TREM",
-        categoria:"TRANSPORTE"
-    },
-    palavra019 = {
-        nome: "CAIAQUE",
-        categoria:"TRANSPORTE"
-    },
-    palavra020 = {
-        nome: "FUNICULAR",
-        categoria:"TRANSPORTE"
-    },
-    palavra021 = {
-        nome: "XICARA",
-        categoria:"OBJETOS"
-    },
-    palavra022 = {
-        nome: "MOEDA",
-        categoria:"OBJETOS"
-    },
-    palavra023 = {
-        nome: "ESPARADRAPO",
-        categoria:"OBJETOS"
-    },
-    palavra024 = {
-        nome: "SINO",
-        categoria:"OBJETOS"
-    },
-    palavra025 = {
-        nome: "CHUVEIRO",
-        categoria:"OBJETOS"
-    },
-    palavra026 = {
-        nome: "TAMBORETE",
-        categoria:"OBJETOS"
-    },
-    palavra027 = {
-        nome: "LAMPADA",
-        categoria:"OBJETOS"
-    },
-    palavra028 = {
-        nome: "BOCAL",
-        categoria:"OBJETOS"
-    },
-    palavra029 = {
-        nome: "CORTINA",
-        categoria:"OBJETOS"
-    },
-    palavra030 = {
-        nome: "LAPIS",
-        categoria:"OBJETOS"
-    },
-    palavra031 = {
-        nome: "MELANCIA",
-        categoria:"ALIMENTOS"
-    },
-    palavra032 = {
-        nome: "AMENDOIM",
-        categoria:"ALIMENTOS"
-    },
-    palavra033 = {
-        nome: "ESFIRRA",
-        categoria:"ALIMENTOS"
-    },
-    palavra034 = {
-        nome: "GOROROBA",
-        categoria:"ALIMENTOS"
-    },
-    palavra035 = {
-        nome: "JANTAR",
-        categoria:"ALIMENTOS"
-    },
-    palavra036 = {
-        nome: "SABOROSO",
-        categoria:"ALIMENTOS"
-    },
-    palavra037 = {
-        nome: "DESJEJUM",
-        categoria:"ALIMENTOS"
-    },
-    palavra038 = {
-        nome: "MASTIGAR",
-        categoria:"ALIMENTOS"
-    },
-    palavra039 = {
-        nome: "ENGOLIR",
-        categoria:"ALIMENTOS"
-    },
-    palavra040 = {
-        nome: "DOCERIA",
-        categoria:"ALIMENTOS"
-    },
-    palavra040 = {
-        nome: "DRAGAO",
-        categoria:"ANIMAIS"
-    },
-    palavra041 = {
-        nome: "GALINHA",
-        categoria:"ANIMAIS"
-    },
-    palavra042 = {
-        nome: "PAVAO",
-        categoria:"ANIMAIS"
-    },
-    palavra043 = {
-        nome: "CAMELO",
-        categoria:"ANIMAIS"
-    },
-    palavra044 = {
-        nome: "PERU",
-        categoria:"ANIMAIS"
-    },
-    palavra045 = {
-        nome: "ZEBRA",
-        categoria:"ANIMAIS"
-    },
-    palavra046 = {
-        nome: "DROMEDARIO",
-        categoria:"ANIMAIS"
-    },
-    palavra047 = {
-        nome: "CALANGO",
-        categoria:"ANIMAIS"
-    },
-    palavra048 = {
-        nome: "SAGUI",
-        categoria:"ANIMAIS"
-    },
-    palavra049 = {
-        nome: "LAGARTIXA",
-        categoria:"ANIMAIS"
-    },
-    palavra050 = {
-        nome: "HIPOPOTAMO",
-        categoria:"ANIMAIS"
-    }
+    { nome: "IRLANDA", categoria: "PAISES" },
+    { nome: "EQUADOR", categoria: "PAISES" },
+    { nome: "CHILE", categoria: "PAISES" },
+    { nome: "INDONESIA", categoria: "PAISES" },
+    { nome: "MALDIVAS", categoria: "PAISES" },
+    { nome: "INGLATERRA", categoria: "PAISES" },
+    { nome: "GROELANDIA", categoria: "PAISES" },
+    { nome: "UZBEQUISTAO", categoria: "PAISES" },
+    { nome: "CREGUENHEM", categoria: "PAISES" },
+    { nome: "BICICLETA", categoria: "TRANSPORTE" },
+    { nome: "LANCHA", categoria: "TRANSPORTE" },
+    { nome: "NAVIO", categoria: "TRANSPORTE" },
+    { nome: "TELEFERICO", categoria: "TRANSPORTE" },
+    { nome: "MOTOCICLETA", categoria: "TRANSPORTE" },
+    { nome: "BARCO", categoria: "TRANSPORTE" },
+    { nome: "AERONAVE", categoria: "TRANSPORTE" },
+    { nome: "TREM", categoria: "TRANSPORTE" },
+    { nome: "CAIAQUE", categoria: "TRANSPORTE" },
+    { nome: "FUNICULAR", categoria: "TRANSPORTE" },
+    { nome: "XICARA", categoria: "OBJETOS" },
+    { nome: "MOEDA", categoria: "OBJETOS" },
+    { nome: "ESPARADRAPO", categoria: "OBJETOS" },
+    { nome: "SINO", categoria: "OBJETOS" },
+    { nome: "CHUVEIRO", categoria: "OBJETOS" },
+    { nome: "TAMBORETE", categoria: "OBJETOS" },
+    { nome: "LAMPADA", categoria: "OBJETOS" },
+    { nome: "BOCAL", categoria: "OBJETOS" },
+    { nome: "CORTINA", categoria: "OBJETOS" },
+    { nome: "LAPIS", categoria: "OBJETOS" },
+    { nome: "MELANCIA", categoria: "ALIMENTOS" },
+    { nome: "AMENDOIM", categoria: "ALIMENTOS" },
+    { nome: "ESFIRRA", categoria: "ALIMENTOS" },
+    { nome: "GOROROBA", categoria: "ALIMENTOS" },
+    { nome: "JANTAR", categoria: "ALIMENTOS" },
+    { nome: "SABOROSO", categoria: "ALIMENTOS" },
+    { nome: "DESJEJUM", categoria: "ALIMENTOS" },
+    { nome: "MASTIGAR", categoria: "ALIMENTOS" },
+    { nome: "ENGOLIR", categoria: "ALIMENTOS" },
+    { nome: "DOCERIA", categoria: "ALIMENTOS" },
+    { nome: "DRAGAO", categoria: "ANIMAIS" },
+    { nome: "GALINHA", categoria: "ANIMAIS" },
+    { nome: "PAVAO", categoria: "ANIMAIS" },
+    { nome: "CAMELO", categoria: "ANIMAIS" },
+    { nome: "PERU", categoria: "ANIMAIS" },
+    { nome: "ZEBRA", categoria: "ANIMAIS" },
+    { nome: "DROMEDARIO", categoria: "ANIMAIS" },
+    { nome: "CALANGO", categoria: "ANIMAIS" },
+    { nome: "SAGUI", categoria: "ANIMAIS" },
+    { nome: "LAGARTIXA", categoria: "ANIMAIS" },
+    { nome: "HIPOPOTAMO", categoria: "ANIMAIS" }
 ];
 
-function criarPalavraSecreta(){
-    const indexPalavra = parseInt(Math.random() * palavras.length)
-    
+function criarPalavraSecreta() {
+    const indexPalavra = Math.floor(Math.random() * palavras.length);
     palavraSecretaSorteada = palavras[indexPalavra].nome;
     palavraSecretaCategoria = palavras[indexPalavra].categoria;
 }
 
-
-function montarPalavraNaTela(){
+function montarPalavraNaTela() {
     const categoria = document.getElementById("categoria");
-    categoria.innerHTML = "Dica: " +palavraSecretaCategoria;
+    categoria.innerHTML = "Dica: " + palavraSecretaCategoria;
 
     const palavraTela = document.getElementById("palavra-secreta");
     palavraTela.innerHTML = "";
-   
-    for(i = 0; i < palavraSecretaSorteada.length; i++){
-        if(listaDinamica[i] == undefined){
-            listaDinamica[i] = "&nbsp;"
-            palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
-        }
-        else{
-            palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
-        }
+
+    for (let i = 0; i < palavraSecretaSorteada.length; i++) {
+        listaDinamica[i] = listaDinamica[i] || "&nbsp;";
+        palavraTela.innerHTML += `<div class='letras'>${listaDinamica[i]}</div>`;
     }
 }
 
-function verificaLetraEscolhida(letra){
+function verificaLetraEscolhida(letra) {
     document.getElementById("tecla-" + letra).disabled = true;
-    if(tentativas > 0)
-    {
+    if (tentativas > 0) {
         mudarStyleLetra("tecla-" + letra);
-        comparalistas(letra);
+        compararListas(letra);
         montarPalavraNaTela();
-    }    
+    }
 }
 
-function mudarStyleLetra(tecla){
+function mudarStyleLetra(tecla) {
     document.getElementById(tecla).style.background = "#002afa";
     document.getElementById(tecla).style.color = "#ffffff";
 }
 
-function comparalistas(letra){
-    const pos = palavraSecretaSorteada.indexOf(letra)
-    if(pos < 0){
-        tentativas--
+function compararListas(letra) {
+    if (palavraSecretaSorteada.indexOf(letra) === -1) {
+        tentativas--;
         carregaImagemForca();
-        if(tentativas < 6){
-          const textoTentativas = document.getElementById("textoTentativas");
-          textoTentativas.innerHTML = "Tentativas Restantes: "+tentativas;  
-          listaLetraErrada[a] = letra;
-          letrasErradas.innerHTML =  "Letras erradas: " +listaLetraErrada;
-          a++;
+        document.getElementById("textoTentativas").innerHTML = `Tentativas Restantes: ${tentativas}`;
+        listaLetraErrada.push(letra);
+        letrasErradas.innerHTML = `Letras erradas: ${listaLetraErrada.join(", ")}`;
 
+        if (tentativas === 0) {
+            abreModal("Você perdeu!", `A palavra secreta era: ${palavraSecretaSorteada}`);
         }
-        if(tentativas == 0){
-            abreModal("Você perdeu KKKKKKKKK", "A palavra secreta era: " + palavraSecretaSorteada);
-        }
-    }
-    else{
-        for(i = 0; i < palavraSecretaSorteada.length; i++){
-            if(palavraSecretaSorteada[i] == letra){
+    } else {
+        for (let i = 0; i < palavraSecretaSorteada.length; i++) {
+            if (palavraSecretaSorteada[i] === letra) {
                 listaDinamica[i] = letra;
             }
         }
     }
-    
-    let vitoria = true;
-  
-    for(i = 0; i < palavraSecretaSorteada.length; i++){
-        if(palavraSecretaSorteada[i] != listaDinamica[i]){
-            vitoria = false;
-    }
-}
 
-    if(vitoria == true)
-    {
-        abreModal("Tá ficando top!", "Você ganhou");
+    if (listaDinamica.join('') === palavraSecretaSorteada) {
+        abreModal("Parabéns!", "Você ganhou!");
         tentativas = 0;
     }
-   
 }
 
-function carregaImagemForca(){
-    switch(tentativas){
-        case 5:
-            document.getElementById("imagem").style.background  = "url('./img/forca01.png')";
-            break;
-        case 4:
-            document.getElementById("imagem").style.background  = "url('./img/forca02.png')";
-            break;
-        case 3:
-            document.getElementById("imagem").style.background  = "url('./img/forca03.png')";
-            break;
-        case 2:
-            document.getElementById("imagem").style.background  = "url('./img/forca04.png')";
-            break;
-        case 1:
-            document.getElementById("imagem").style.background  = "url('./img/forca05.png')";
-            break;
-        case 0:
-            document.getElementById("imagem").style.background  = "url('./img/forca06.png')";
-            break;
-        case 6:
-            document.getElementById("imagem").style.background  = "url('./img/forca.png')";
-            break;
-        default:
-
-            break;
-    }
+function carregaImagemForca() {
+    const imagens = [
+        './img/forca06.png',
+        './img/forca05.png',
+        './img/forca04.png',
+        './img/forca03.png',
+        './img/forca02.png',
+        './img/forca01.png',
+        './img/forca.png'
+    ];
+    document.getElementById("imagem").style.background = `url('${imagens[tentativas]}')`;
 }
 
-function abreModal(titulo, mensagem){
-    let modalTitulo = document.getElementById("exampleModalLabel");
-    modalTitulo.innerText = titulo;
-
-    let modalBody = document.getElementById("modalBody");
-    modalBody.innerHTML = mensagem;
+function abreModal(titulo, mensagem) {
+    document.getElementById("exampleModalLabel").innerText = titulo;
+    document.getElementById("modalBody").innerHTML = mensagem;
 
     $("#myModal").modal({
         show: true
     });
 }
 
-let bntReiniciar = document.querySelector("#btnReiniciar")
-bntReiniciar.addEventListener("click", function(){
+document.getElementById("btnReiniciar").addEventListener("click", function () {
     location.reload();
 });
 
-let bntStart= document.querySelector("#btnStart")
-  bntStart.addEventListener("click", function(){
-  document.getElementById("teclado").style.visibility = "visible";
-  criarPalavraSecreta();
-  montarPalavraNaTela();
-  carregaImagemForca();
+document.getElementById("btnStart").addEventListener("click", function () {
+    document.getElementById("teclado").style.visibility = "visible";
+    criarPalavraSecreta();
+    montarPalavraNaTela();
+    carregaImagemForca();
 });
